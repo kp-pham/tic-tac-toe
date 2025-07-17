1ff70c31e75af7519c2e5717b43fdf2efd58357e
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-07-16 20:55:55 -0700

    refactor: create private helper functions for marking space
    Check whether the place is empty and assign the mark to the cell of the two-dimensional array.

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 function createGameboard() {
-    const rows = 3;
-    const columns = 3;
-    const board = []
+    const EMPTY = '';
+    const ROWS = 3;
+    const COLUMNS = 3;
+
+    const board = [];
     
-    for (let i = 0; i < rows; ++i) {
+    for (let i = 0; i < ROWS; ++i) {
         board[i] = [];
     
-        for (let j = 0; j < columns; ++j) {
-            board[i].push('');
+        for (let j = 0; j < COLUMNS; ++j) {
+            board[i].push(EMPTY);
         }
     }
 
     const getBoard = () => board;
-    const markSpace = (row, column, mark) => {
-        board[row][column] = mark;
+
+    const markSpace = function (row, column, mark) {
+        if (spaceEmpty(row, column))
+            placeMark(row, column, mark);
+        else
+            console.log("Cannot place mark on space which is not empty.");
     }
+
+    const spaceEmpty = (row, column) => board[row][column] === EMPTY;
+    const placeMark = (row, column, mark) => board[row][column] = mark;
+    
 
     return { board, getBoard, markSpace };
 }
