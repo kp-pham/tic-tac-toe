0c768467546ed3c7c74915712cfe5a772a61a09d
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-07-16 21:15:11 -0700

    refactor: create helper function for bounds checking
    Prevent negative rows and columns and rows and columns greater than the bounds.

--- index.js
+++ index.js
@@ -16,15 +16,19 @@ function createGameboard() {
     const getBoard = () => board;
 
     const markSpace = function (row, column, mark) {
-        if (spaceEmpty(row, column))
-            placeMark(row, column, mark);
-        else
+        if (!inBounds(row, column))
+            console.log("Cannot place mark on space out of bounds.");
+        else if (!spaceEmpty(row, column))
             console.log("Cannot place mark on space which is not empty.");
+        else  
+            placeMark(row, column, mark);
     }
 
+    const inBounds = (row, column) => rowInBounds(row) && columnInBounds(column);
+    const rowInBounds = row => row >= 0 && row <= ROWS - 1;
+    const columnInBounds = column => column >= 0 && column <= COLUMNS - 1;
     const spaceEmpty = (row, column) => board[row][column] === EMPTY;
     const placeMark = (row, column, mark) => board[row][column] = mark;
-    
 
     return { board, getBoard, markSpace };
 }
